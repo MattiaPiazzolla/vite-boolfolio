2d2eb14762b318fd3d46d029d4992b4141f3f2ea
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-15 18:18:31 +0200

    [UPD]added images

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {store} from 'vue'
+
+export const store = reactive({
+    menuItems: [
+        {
+            name: 'homepage',
+            label: 'Home'
+        },
+        {
+            name: 'projects',
+            label
+        }
+    ]
+});
