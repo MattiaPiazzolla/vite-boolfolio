0d7aacc832cea46e5bc37036a19a8c053194c10f
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-16 17:17:24 +0200

    [FIX] problem

--- src/pages/PageProject.vue
+++ src/pages/PageProject.vue
@@ -24,16 +24,20 @@ export default {
 };
 </script>
 <template>
-	<h1>{{ project.name }}</h1>
+	<h1>{{ project.name || "Caricamento..." }}</h1>
+
 	<img
 		:src="
-			project.project_image.startsWith('http')
+			project.project_image?.startsWith('http')
 				? project.project_image
 				: `http://127.0.0.1:8000/storage/${project.project_image}`
 		"
-		class="card-img-top"
-		:alt="`${project.name} image`" />
+		:alt="`${project.name || 'Immagine del progetto'}`" />
+	<p>{{ project.summary || "Nessuna descrizione disponibile." }}</p>
 </template>
-<style
-	lang="scss
-"></style>
+<style lang="scss" scoped>
+img {
+	max-width: 200px;
+	aspect-ratio: 1/1;
+}
+</style>
