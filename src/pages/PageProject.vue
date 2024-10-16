5f775a403937de9f0cfd67f8d0ccd70865e984b9
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-16 17:26:08 +0200

    [UPD]added more information

--- src/pages/PageProject.vue
+++ src/pages/PageProject.vue
@@ -24,16 +24,27 @@ export default {
 };
 </script>
 <template>
-	<h1>{{ project.name || "Caricamento..." }}</h1>
+	<div>
+		<h1>{{ project.name || "Caricamento..." }}</h1>
+		<img
+			:src="
+				project.project_image?.startsWith('http')
+					? project.project_image
+					: `http://127.0.0.1:8000/storage/${project.project_image}`
+			"
+			:alt="`${project.name || 'Immagine del progetto'}`" />
+		<p>{{ project.summary || "Nessuna descrizione disponibile." }}</p>
 
-	<img
-		:src="
-			project.project_image?.startsWith('http')
-				? project.project_image
-				: `http://127.0.0.1:8000/storage/${project.project_image}`
-		"
-		:alt="`${project.name || 'Immagine del progetto'}`" />
-	<p>{{ project.summary || "Nessuna descrizione disponibile." }}</p>
+		<div>
+			<p><strong>Categoria:</strong> {{ project.category?.name || "N/A" }}</p>
+			<p><strong>Tecnologie:</strong></p>
+			<ul>
+				<li v-for="tech in project.technologies" :key="tech.id">
+					{{ tech.name }}
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
 <style lang="scss" scoped>
 img {
